397d1afce5cabbfec9edb95281dd9e0c9a80d096
Will Worth <will.worth@gmail.com>
2018-08-18 10:32:22 +0200

    blog integrated!

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,28 +1,75 @@
 module.exports = {
   siteMetadata: {
     title: 'willworth.net',
+    author: 'Will Worth',
+    description: 'Personal site of musician and writer Will Worth.',
+    siteUrl: 'https://www.willworth.net/',
   },
-  plugins: [
-    'gatsby-plugin-react-helmet',
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
-        start_url: '/',
-        background_color: '#663399',
-        theme_color: '#bdc667',
-        display: 'minimal-ui',
-        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
+
+    pathPrefix: '/gatsby-starter-blog',
+    plugins: [
+      {
+        resolve: `gatsby-source-filesystem`,
+        options: {
+          path: `${__dirname}/src/pages`,
+          name: 'pages',
+        },
       },
-    },
-    'gatsby-plugin-offline',
-    {
-      resolve: `gatsby-plugin-typography`,
-      options: {
-        pathToConfigModule: `src/utils/typography.js`,
+      {
+        resolve: `gatsby-transformer-remark`,
+        options: {
+          plugins: [
+            {
+              resolve: `gatsby-remark-images`,
+              options: {
+                maxWidth: 590,
+              },
+            },
+            {
+              resolve: `gatsby-remark-responsive-iframe`,
+              options: {
+                wrapperStyle: `margin-bottom: 1.0725rem`,
+              },
+            },
+            'gatsby-remark-prismjs',
+            'gatsby-remark-copy-linked-files',
+            'gatsby-remark-smartypants',
+          ],
+        },
       },
-    },
+      `gatsby-transformer-sharp`,
+      `gatsby-plugin-sharp`,
+      {
+        resolve: `gatsby-plugin-google-analytics`,
+        options: {
+          //trackingId: `ADD YOUR TRACKING ID HERE`,
+        },
+      },
+      `gatsby-plugin-feed`,
+      {
+        resolve: `gatsby-plugin-manifest`,
+        options: {
+          name: 'gatsby-starter-default',
+          short_name: 'starter',
+          start_url: '/',
+          background_color: '#663399',
+          theme_color: '#bdc667',
+          display: 'minimal-ui',
+          icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
+        },
+      },
+      `gatsby-plugin-offline`,
+      `gatsby-plugin-react-helmet`,
+      {
+        resolve: 'gatsby-plugin-typography',
+        options: {
+          pathToConfigModule: 'src/utils/typography',
+        },
+      },
+    ],
+  }
   
-  ],
-}
+    
+  
+
+
--- src/utils/typography.js
+++ src/utils/typography.js
@@ -5,8 +5,12 @@ import altonTheme from 'typography-theme-alton'
 //https://coolors.co/bdc667-11151c-585563-33261d-ccdad1
 const typography = new Typography(altonTheme)
 
-
+if (process.env.NODE_ENV !== 'production') {
+    typography.injectStyles()
+  }
 
 export default typography
 
 
+export const rhythm = typography.rhythm
+export const scale = typography.scale
